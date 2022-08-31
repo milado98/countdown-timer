85c75276ccd453eb1680e800aef37f35ecd6f9eb
Emmanuel Chima <chimaemma04@gmail.com>
2022-08-31 07:24:42 +0100

    Add files via upload
    created a countdown timer

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const daysElement = document.getElementById('days');
+const hoursElement = document.getElementById('hours');
+const minutesElement = document.getElementById('minutes');
+const secondsElement = document.getElementById('seconds');
+
+const birthday = '29 Nov 2022';
+
+function countdown() {
+    const birthdayDate = new Date(birthday);
+    const currentDate = new Date ();
+
+    const totalSeconds = (birthdayDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysElement.innerHTML = formatTime(days);
+    hoursElement.innerHTML = formatTime(hours);
+    minutesElement.innerHTML = formatTime(minutes);
+    secondsElement.innerHTML = formatTime(seconds);
+
+    
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+countdown();
+
+setInterval(countdown, 1000);
